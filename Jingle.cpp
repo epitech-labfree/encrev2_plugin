79efa82d2cee3d2e12d794a41aa6aec19d1f13a9
tribou_f <tribou_f@epitech.net>
2011-01-06 16:10:31 +0100

    debut de jingle

--- Jingle.cpp
+++ Jingle.cpp
@@ -14,3 +14,47 @@ Jingle::~Jingle()
 {
   cout << "Libjingle : destroyed" << endl;
 }
+
+void		Jingle::init(std::string username)
+{
+  //thread la lib
+#if WIN32
+  talk_base::Win32Thread w32_thread;
+  talk_base::ThreadManager::SetCurrent(&w32_thread);
+#endif
+
+  talk_base::Thread* main_thread = talk_base::Thread::Current();
+
+  // buzz::Jid				jid;
+  // buzz::XmppClientSettings		xcs;
+  // talk_base::InsecureCryptStringImpl	pass;
+  _username = username;
+  _clientJid = buzz::Jid(username);
+  if (!_clientJid.IsValid() || _clientJid.node() == "") {
+    printf("jid invalid : username=login@doamin\n");
+    return ;
+  }
+
+  buzz::XmppClientSettings xcs;
+  xcs.set_user(_clientJid.node());
+  xcs.set_resource("call");
+  xcs.set_host(_clientJid.domain());
+  xcs.set_use_tls(!true);
+  xcs.set_use_tls(false);
+  xcs.set_allow_plain(true);
+
+  std::string	host;
+  short		port;
+  int		pos = _server.find(':');
+  if (pos == -1)
+    {
+      host = _server;
+      port = 5222;
+    }
+  else
+    {
+      host = _server.substr(0, pos);
+      port = atoi(_server.substr(pos + 1).c_str());
+    }
+  xcs.set_server(talk_base::SocketAddress(host, port));
+}
--- Jingle.hh
+++ Jingle.hh
@@ -1,13 +1,41 @@
 #ifndef   	JINGLE_HH_
 # define   	JINGLE_HH_
 
+#include <time.h>
+#include <iomanip>
+#include <cstdio>
+#include <cstring>
+#include <vector>
 #include "talk/base/ssladapter.h"
+#include "talk/base/logging.h"
+#include "talk/base/flags.h"
+#include "talk/base/pathutils.h"
+#include "talk/base/stream.h"
+#include "talk/base/win32socketserver.h"
+#include "talk/p2p/base/constants.h"
+#include "talk/xmpp/xmppclientsettings.h"
+#include "talk/p2p/base/constants.h"
+#include "talk/xmpp/xmppclientsettings.h"
+#include "talk/examples/login/xmppthread.h"
+#include "talk/examples/login/xmppauth.h"
+#include "talk/examples/login/xmpppump.h"
 
 class Jingle
 {
-public:
+public :
   Jingle();
   ~Jingle();
+  void		init(std::string);
+
+  void WakeTasks() {};
+  int64 CurrentTime() {};
+  void OnMessage(talk_base::Message *pmsg) {};
+
+private :
+  std::string		_username;
+  std::string		_server;
+  std::string		_jid;
+  buzz::Jid		_clientJid;
 };
 
 #endif	    /* !JINGLE_HH_ */
