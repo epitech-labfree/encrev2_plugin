38bc56cceb2947e635743cdb87156197ea894f26
tribou_f <tribou_f@epitech.net>
2011-02-17 16:00:35 +0100

    replacement of encrev2_vlc by encre, to be generic without vlc

---
+++ encre.cpp
@@ -0,0 +1,65 @@
+#include "encre.hh"
+
+#include <iostream>
+
+template <typename T>
+const char*	encre::Encre<T>::DefaultArgs[] =
+  {
+    "-I",
+    "--ignore-config",
+    "dummy",
+    "-vv"
+  };
+
+namespace encre
+{
+  template <typename T>
+  Encre<T>::Encre() : m_vlc(0), m_stream(0)
+  {
+    std::clog << "Unknown ype" << std::endl;
+  }
+
+  template<>
+  Encre<libvlc_instance_t>::Encre() : m_vlc(0), m_stream(0)
+  {
+    m_vlc = libvlc_new(sizeof(DefaultArgs) / sizeof(*DefaultArgs), DefaultArgs);
+  }
+
+  template <typename T>
+  Encre<T>::Encre(const std::vector<std::string>& vlcOpts) :m_vlc(0), m_stream(0)
+  {
+    std::clog << "Unknown type" << std::endl;
+  }
+
+  template <>
+  Encre<libvlc_instance_t>::Encre(const std::vector<std::string>& vlcOpts) :m_vlc(0), m_stream(0)
+  {
+    const char*	vlcArgs[vlcOpts.size()];
+    for (unsigned int i=0; i < vlcOpts.size(); ++i)
+      {
+	vlcArgs[i] = vlcOpts[i].c_str();
+      }
+    m_vlc = libvlc_new(sizeof(vlcArgs) / sizeof(*vlcArgs), vlcArgs);
+  }
+
+  template <typename T>
+  Stream*	Encre<T>::getStream() const
+  {
+    return m_stream;
+  }
+  template Stream*	Encre<libvlc_instance_t>::getStream() const;
+
+  template <typename T>
+  bool			Encre<T>::start()
+  {
+    return true;
+  }
+
+  template <>
+  bool			Encre<libvlc_instance_t>::start()
+  {
+    std::cout << "here we initilise the network, return depend on it" << std::endl;
+    return true;
+  }
+
+}
---
+++ encre.hh
@@ -0,0 +1,34 @@
+#ifndef _ENCRE_H
+# define _ENCRE_H
+
+#include <string>
+#include <vector>
+#include <boost/utility.hpp>
+#include <vlc/vlc.h>
+
+namespace encre
+{
+  class	Stream;
+
+  template <typename T>
+  class	Encre : public boost::noncopyable
+  {
+  private:
+    T*						m_vlc;
+    Stream*					m_stream;
+
+    static const char*	DefaultArgs[];
+
+  public :
+    Encre();
+    Encre(const std::vector<std::string>&	vlcOpts);
+
+    bool					start();
+    Stream*					getStream() const;
+    friend					class Stream;
+  };
+}
+
+#include "Stream.hh"
+
+#endif // _ENCRE_H
