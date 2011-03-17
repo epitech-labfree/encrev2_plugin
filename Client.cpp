7b53c28d39a59ed438b77dbedd2eed9cdfb1f539
tribou_f <tribou_f@epitech.net>
2011-03-17 14:44:15 +0100

    resolve a bug

--- Client.cpp
+++ Client.cpp
@@ -8,8 +8,9 @@ void
 Client::get_data(char** data, size_t* size) {
 	// XXX: Check for brain dead people who don't call is_data_received
  	for (unsigned int i = 0; i < m_buff->size(); ++i) {
-	  *data[i] = m_buff->at(i);
+	  (*data)[i] = m_buff->at(i);
 	}
 	*size = m_buff->size();
 	delete m_buff;
+	m_buff = 0;
 }
